11a646508c2997362c9cfa52871b57c327da435a
2nky <121765538+2nky@users.noreply.github.com>
2023-07-17 21:24:44 +0200

    Добавляем поле статуса рассмотрения заявки.

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,21 @@
-from django.db.models import Model, DateTimeField, BinaryField, JSONField
+from django.db.models import Model, DateTimeField, BinaryField, JSONField, CharField
 
 
 class MountainCrossing(Model):
     date_added = DateTimeField(verbose_name="Дата создания")
     raw_data = JSONField(verbose_name="Сырые данные")
     images = JSONField(verbose_name="Список изображений")
+    status = CharField(
+        max_length=16,
+        verbose_name="Статус заявки",
+        default="new",
+        choices=(
+            ("new", "Новое"),
+            ("pending", "В работе"),
+            ("accepted", "Принято"),
+            ("rejected", "Отказано"),
+        ),
+    )
 
     class Meta:
         db_table = "pereval_added"
